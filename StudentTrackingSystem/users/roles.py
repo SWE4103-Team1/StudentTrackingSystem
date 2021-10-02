075333f9deb353fa72840c68c856c812475fad5b
Justen Di Ruscio <justend29@gmail.com>
2021-10-02 19:54:12 -0300

    move user models to users app

---
+++ StudentTrackingSystem/users/roles.py
@@ -0,0 +1,7 @@
+from enum import IntEnum
+
+
+class UserRole(IntEnum):
+    ProgramAdvisor = 0
+    ProgramCoordinator = 1
+    AccredidationCoordinator = 2
---
+++ StudentTrackingSystem/users/tests.py
@@ -0,0 +1,59 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+from .roles import UserRole
+
+
+class UserManagerTests(TestCase):
+    def test_create_basic_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(email="normal@user.com", password="foo")
+        self.assertEqual(user.email, "normal@user.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+        try:
+            self.assertIsNotNone(user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(TypeError):
+            User.objects.create_user()
+        with self.assertRaises(TypeError):
+            User.objects.create_user(email="")
+        with self.assertRaises(ValueError):
+            User.objects.create_user(email="", password="foo")
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            email="normal@user.com",
+            password="foo",
+            username="normal",
+            role=UserRole.AccredidationCoordinator,
+        )
+        self.assertEqual(user.email, "normal@user.com")
+        self.assertEqual(user.username, "normal")
+        self.assertEqual(user.role, UserRole.AccredidationCoordinator)
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_basic_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            email="super@user.com", password="foo"
+        )
+        self.assertEqual(admin_user.email, "super@user.com")
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+
+        try:
+            self.assertIsNotNone(admin_user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                email="super@user.com", password="foo", is_superuser=False
+            )
