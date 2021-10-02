64922217e3db69c5f0c291449aae7c5a5dfbb3d6
Justen Di Ruscio <justend29@gmail.com>
2021-10-02 20:00:02 -0300

    set User to auth user model

--- StudentTrackingSystem/StudentTrackingSystem/settings.py
+++ StudentTrackingSystem/StudentTrackingSystem/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -127,4 +128,4 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-AUTH_USER_MODEL = "StudentTrackingSystemApp.User"
+AUTH_USER_MODEL = "users.User"
--- StudentTrackingSystem/StudentTrackingSystemApp/user_roles.py
+++
@@ -1,7 +0,0 @@
-from enum import IntEnum
-
-
-class UserRole(IntEnum):
-    ProgramAdvisor = 0
-    ProgramCoordinator = 1
-    AccredidationCoordinator = 2
