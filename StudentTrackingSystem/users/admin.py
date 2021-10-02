3e4080a7e78317d2608973943838d351b07d3e53
Justen Di Ruscio <justend29@gmail.com>
2021-10-02 20:03:47 -0300

    move admin mapping to users app

--- StudentTrackingSystem/users/admin.py
+++ StudentTrackingSystem/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import User
+
+admin.site.register(User, UserAdmin)
+
+# TODO: may need to create a custom UserAdmin such that it displays all fields from the custom User model
