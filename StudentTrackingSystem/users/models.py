b042a128c6470de4b464089877769e5267ef6013
Justen Di Ruscio <justend29@gmail.com>
2021-10-02 19:59:21 -0300

    User inherets from AbstractUser

--- StudentTrackingSystem/users/models.py
+++ StudentTrackingSystem/users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from .managers import UserManager
+
+
+class User(AbstractUser):
+    # username & password from AbstractUser
+    email = models.EmailField(verbose_name="email address", unique=True, max_length=255)
+    role = models.IntegerField()
+
+    USERNAME_FIELD = (
+        "email"  # makes email the primary key for default authentication backend
+    )
+
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = ["role"]  # in addition to USERNAME_FIELD and password
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
