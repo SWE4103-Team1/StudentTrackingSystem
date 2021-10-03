2516fff3a6a4941d4ceae2f9c073fd0998475718
Yuzhuo Zheng <yzheng10@unb.ca>
2021-10-03 15:37:28 -0300

    create models of Course and Enrolment, then add relationships

--- StudentTrackingSystem/StudentTrackingSystemApp/models.py
+++ StudentTrackingSystem/StudentTrackingSystemApp/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+
+class Student(models.Model):
+    sid = models.IntegerField(max_length=8, primary_key=True)
+    name = models.CharField(max_length=70)
+    gender = models.CharField(max_length=1)
+    address = models.TextField(max_length=140)
+    email = models.EmailField(max_length=50)
+    campus = models.CharField(max_length=2)
+    program = models.CharField(max_length=10)
+    start_date = models.DateField(max_length=8)
+
+
 class Course(models.Model):
     course_code = models.CharField(max_length=20, primary_key=True)
     section = models.CharField(max_length=10, primary_key=True)
@@ -14,12 +26,4 @@ class Enrolment(models.Model):
     grade = models.CharField(max_length=5)
     term = models.CharField(max_length=10)
 
-class Student(models.Model):
-        sid = models.IntegerField(max_length=8, primary_key=True)
-        name = models.CharField(max_length=70)
-        gender = models.CharField(max_length=1)
-        address = models.TextField(max_length=140)
-        email = models.EmailField(max_length=50)
-        campus = models.CharField(max_length=2)
-        program = models.CharField(max_length=10)
-        start_date = models.DateField(max_length=8)
+
