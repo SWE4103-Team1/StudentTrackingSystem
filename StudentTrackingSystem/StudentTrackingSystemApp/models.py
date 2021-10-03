68f2ead621f51b40d755ab3b788d1b39760eeb0d
Yuzhuo Zheng <yzheng10@unb.ca>
2021-10-03 16:41:14 -0300

    create manager of course/enrolment

--- StudentTrackingSystem/StudentTrackingSystemApp/models.py
+++ StudentTrackingSystem/StudentTrackingSystemApp/models.py
@@ -11,12 +11,18 @@ class Student(models.Model):
     program = models.CharField(max_length=10)
     start_date = models.DateField(max_length=8)
 
+    def __str__(self):
+        return self.sid
+
 
 class Course(models.Model):
     course_code = models.CharField(max_length=20, primary_key=True)
     section = models.CharField(max_length=10, primary_key=True)
     credit_hours = models.IntegerField()
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.course_code
 
 
 class Enrolment(models.Model):
@@ -26,4 +32,5 @@ class Enrolment(models.Model):
     grade = models.CharField(max_length=5)
     term = models.CharField(max_length=10)
 
-
+    def __str__(self):
+        return self.sid
