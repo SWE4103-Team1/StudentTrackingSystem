f583d4a325a70f9115df1135bf8a0dfa89a8af3e
Justen Di Ruscio <justend29@gmail.com>
2021-10-04 16:43:00 -0300

    print correct student information

--- StudentTrackingSystem/datamodel/tests.py
+++ StudentTrackingSystem/datamodel/tests.py
@@ -3,15 +3,6 @@ from .models import Student
 
 from datetime import date
 
-# sid = models.IntegerField(primary_key=True)
-# name = models.CharField(max_length=70)
-# gender = models.CharField(max_length=1)
-# address = models.TextField(max_length=140)
-# email = models.EmailField(max_length=50)
-# campus = models.CharField(max_length=2)
-# program = models.CharField(max_length=10)
-# start_date = models.DateField(max_length=8)
-
 
 class StudentTests(TestCase):
     def test_create_student(self):
@@ -26,4 +17,4 @@ class StudentTests(TestCase):
             start_date=date(2020, 10, 4),
         )
         student.save()
-        # self.assertEqual(student.sid, 123456)
+        self.assertEqual(student.sid, 123456)
