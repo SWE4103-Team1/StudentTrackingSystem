92244e457cd870129a4a009ba7add3fcd3953154
Elliot-Chin <48703126+Elliot-Chin@users.noreply.github.com>
2021-10-05 11:25:04 -0300

    Written test cases

--- StudentTrackingSystem/datamodel/models.py
+++ StudentTrackingSystem/datamodel/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Student(models.Model):
     sid = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=70)
+    name = models.TextField(max_length=70)
     gender = models.CharField(max_length=1)
     address = models.TextField(max_length=140)
     email = models.EmailField(max_length=50)
@@ -12,16 +12,13 @@ class Student(models.Model):
     start_date = models.DateField(max_length=8)
 
     def __str__(self):
-        return self.sid
+        return str(self.sid)
 
 
 class Course(models.Model):
     course_code = models.CharField(max_length=20, primary_key=True)
     credit_hours = models.IntegerField()
     name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
 
 
 class CourseSection(models.Model):
--- StudentTrackingSystem/datamodel/tests.py
+++ StudentTrackingSystem/datamodel/tests.py
@@ -1,5 +1,7 @@
-from django.test import TestCase
-from .models import Student
+from django.db.models.query_utils import select_related_descend
+#from django.test import TestCase
+from unittest import TestCase
+from .models import Student, Course, Enrolment, CourseSection
 
 from datetime import date
 
@@ -13,8 +15,9 @@ from datetime import date
 # start_date = models.DateField(max_length=8)
 
 
-class StudentTests(TestCase):
+class Tests(TestCase):
     def test_create_student(self):
+        global student
         student = Student(
             sid=123456,
             name="john doe",
@@ -26,4 +29,34 @@ class StudentTests(TestCase):
             start_date=date(2020, 10, 4),
         )
         student.save()
-        # self.assertEqual(student.sid, 123456)
+  
+    def test_create_course(self):
+        course = Course(
+            course_code = "SWE4103",
+            credit_hours = 3,
+            name = "This Class Name",
+        )
+        course.save()
+
+    def test_create_courseSection(self):
+        selected_course = [course for course in Course.objects.all() if course.course_code == "SWE4103"]
+        print('xxxxxxxxxxxxxxxxxxxxx' + str(selected_course))
+        course_section = CourseSection(
+            section = "FR01B",
+            course_code = selected_course[0],
+        )
+        course_section.save()
+
+    def test_create_enrollment(self):  
+        selected_course_section = [courseSection for courseSection in CourseSection.objects.all() if courseSection.section == "FR01B"]
+        selected_student = [student for student in Student.objects.all() if student.sid == 123456]
+        enrollment = Enrolment(
+            sid = selected_student[0],
+            course_section = selected_course_section[0],
+            term = "WI2021",
+            grade = "F",
+        )
+        enrollment.save()
+        
+
+    
