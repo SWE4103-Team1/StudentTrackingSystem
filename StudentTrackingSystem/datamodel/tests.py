cd4635393891c2b51615fa81bfdcc49dd77d5773
Yuzhuo Zheng <yzheng10@unb.ca>
2021-10-05 11:29:46 -0300

    write tests

--- StudentTrackingSystem/datamodel/tests.py
+++ StudentTrackingSystem/datamodel/tests.py
@@ -1,21 +1,29 @@
-from django.test import TestCase
+# from django.test import TestCase
+from unittest import TestCase
 from .models import Student
-
+from .models import Course
 from datetime import date
 
 
+def checkEmpty(self, m):
+    all_entries = m.objects.all()
+    self.assertFalse(not all_entries)
+
+
 class StudentTests(TestCase):
-    def test_create_student(self):
-        student = Student(
-            sid=123456,
-            name="john doe",
-            gender="M",
-            address="123 cool street",
-            email="john@unb.ca",
-            campus="FR",
-            program="Computer Science",
-            start_date=date(2020, 10, 4),
-        )
-        student.save()
-        self.assertEqual(student.sid, 123456)
-        self.assertEqual(student.name, "john doe")
+    def test_checkEmpty_student(self):
+        checkEmpty(self, Student)
+        # all_entries = Student.objects.all()
+        # print(all_entries)
+        # self.assertFalse(not all_entries)
+
+    def test_checkEmail_Student(self):
+        all_entries = Student.objects.all()
+        self.assertTrue(x for x in all_entries if x.sid == 123456)
+
+
+class CourseTests(TestCase):
+    def test_checkEmpty_course(self):
+        checkEmpty(self, Course)
+
+
