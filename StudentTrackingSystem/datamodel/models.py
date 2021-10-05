c9a65f8bfcd22ee1212aa3582556b4004c275f2d
Justen Di Ruscio <justend29@gmail.com>
2021-10-05 18:41:26 -0300

    use explicit indices on data model

--- StudentTrackingSystem/datamodel/models.py
+++ StudentTrackingSystem/datamodel/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 
+class UploadSet(models.Model):
+    upload_date = models.DateField(primary_key=True)
+    # must be sure that
+    person_data_file = models.FileField(upload_to="uploads/")
+    course_data_file = models.FileField(upload_to="uploads/")
+    transfer_data_file = models.FileField(upload_to="uploads/")
+
+
 class Student(models.Model):
-    sid = models.IntegerField(primary_key=True)
+    id = models.BigAutoField(primary_key=True)
+    student_number = models.IntegerField()
     name = models.TextField(max_length=70)
     gender = models.CharField(max_length=1)
     address = models.TextField(max_length=140)
@@ -10,32 +19,57 @@ class Student(models.Model):
     campus = models.CharField(max_length=2)
     program = models.CharField(max_length=10)
     start_date = models.DateField(max_length=8)
+    upload_date = models.ForeignKey(UploadSet, on_delete=models.CASCADE)
+
+    class Meta:
+        _index_fields = ["student_number", "upload_date"]
+        indexes = [
+            models.Index(fields=_index_fields, name="student_index"),
+        ]
+        constraints = [
+            models.UniqueConstraint(
+                fields=_index_fields,
+                name="unique student index constraint",
+            )
+        ]
 
     def __str__(self):
         return f"sid: {self.sid}, name: {self.name}, gender: {self.gender}, address: {self.address}, email: {self.email}"
 
 
 class Course(models.Model):
-    course_code = models.CharField(max_length=20, primary_key=True)
+    id = models.BigAutoField(primary_key=True)
+    course_code = models.CharField(max_length=20)
+    section = models.CharField(max_length=10)
     credit_hours = models.IntegerField()
     name = models.CharField(max_length=50)
+    upload_date = models.ForeignKey(UploadSet, on_delete=models.CASCADE)
 
-
-class CourseSection(models.Model):
-    section = models.CharField(max_length=10, primary_key=True)
-    course_code = models.ForeignKey(Course, on_delete=models.CASCADE)
+    class Meta:
+        _index_fields = ["course_code", "section", "upload_date"]
+        indexes = [
+            models.Index(fields=_index_fields, name="course_index"),
+        ]
+        constraints = [
+            models.UniqueConstraint(
+                fields=_index_fields,
+                name="unique_course_index_constraint",
+            )
+        ]
 
 
 class Enrolment(models.Model):
-    sid = models.ForeignKey(Student, on_delete=models.CASCADE)
-    course_section = models.ForeignKey(CourseSection, on_delete=models.CASCADE)
+    id = models.BigAutoField(primary_key=True)
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
     term = models.CharField(max_length=10)
     grade = models.CharField(max_length=5)
+    upload_date = models.ForeignKey(UploadSet, on_delete=models.CASCADE)
 
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=["sid", "course_section"], name="unique enrolment key"
+                fields=["student_id", "course_id"], name="unique_enrolment_constraint"
             )
         ]
 
--- StudentTrackingSystem/datamodel/tests.py
+++ StudentTrackingSystem/datamodel/tests.py
@@ -1,7 +1,6 @@
-
-
 from django.db.models.query_utils import select_related_descend
-#from django.test import TestCase
+
+# from django.test import TestCase
 from unittest import TestCase
 from .models import Student, Course, Enrolment, CourseSection
 
@@ -11,6 +10,7 @@ from datetime import date
 def checkEmpty(self, m):
     all_entries = m.objects.all()
     self.assertFalse(not all_entries)
+
 
 class Tests(TestCase):
     def test_create_student(self):
@@ -26,35 +26,44 @@ class Tests(TestCase):
             start_date=date(2020, 10, 4),
         )
         student.save()
-  
+
     def test_create_course(self):
         course = Course(
-            course_code = "SWE4103",
-            credit_hours = 3,
-            name = "This Class Name",
+            course_code="SWE4103",
+            credit_hours=3,
+            name="This Class Name",
         )
         course.save()
 
     def test_create_courseSection(self):
-        selected_course = [course for course in Course.objects.all() if course.course_code == "SWE4103"]
-        print('xxxxxxxxxxxxxxxxxxxxx' + str(selected_course))
+        selected_course = [
+            course for course in Course.objects.all() if course.course_code == "SWE4103"
+        ]
+        print("xxxxxxxxxxxxxxxxxxxxx" + str(selected_course))
         course_section = CourseSection(
-            section = "FR01B",
-            course_code = selected_course[0],
+            section="FR01B",
+            course_code=selected_course[0],
         )
         course_section.save()
 
-    def test_create_enrollment(self):  
-        selected_course_section = [courseSection for courseSection in CourseSection.objects.all() if courseSection.section == "FR01B"]
-        selected_student = [student for student in Student.objects.all() if student.sid == 123456]
+    def test_create_enrollment(self):
+        selected_course_section = [
+            courseSection
+            for courseSection in CourseSection.objects.all()
+            if courseSection.section == "FR01B"
+        ]
+        selected_student = [
+            student for student in Student.objects.all() if student.sid == 123456
+        ]
         enrollment = Enrolment(
-            sid = selected_student[0],
-            course_section = selected_course_section[0],
-            term = "WI2021",
-            grade = "F",
+            sid=selected_student[0],
+            course_section=selected_course_section[0],
+            term="WI2021",
+            grade="F",
         )
         enrollment.save()
-        
+
+
 class StudentTests(TestCase):
     def test_checkEmpty_student(self):
         checkEmpty(self, Student)
@@ -70,6 +79,3 @@ class StudentTests(TestCase):
 class CourseTests(TestCase):
     def test_checkEmpty_course(self):
         checkEmpty(self, Course)
-    
-
-
