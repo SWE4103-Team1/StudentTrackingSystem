24ea5bc1bf411faf475aa7ed77afbd306e3acedb
EthanG78 <ethangarnier78@gmail.com>
2021-10-09 10:54:30 -0300

    Added code required for django to serve static files

--- StudentTrackingSystem/StudentTrackingSystem/settings.py
+++ StudentTrackingSystem/StudentTrackingSystem/settings.py
@@ -118,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'StudentTrackingSystemApp/static/'
+
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR/'StudentTrackingSystemApp', 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
