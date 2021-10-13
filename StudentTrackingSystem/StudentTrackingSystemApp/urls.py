3d4786b726c7a3ecede3ae0798a9c3115425d206
Elliot-Chin <ychin@unb.ca>
2021-10-13 13:22:47 -0300

    Added function to upload files

--- StudentTrackingSystem/StudentTrackingSystemApp/urls.py
+++ StudentTrackingSystem/StudentTrackingSystemApp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('login/', views.loginPage, name='login'),
-    	path('register/', views.registerPage, name='register')
+    path('login/', views.loginPage, name='login'),
+    path('register/', views.registerPage, name='register'),
+    path('homepage/', views.homePage, name='homepage')
 ]
--- StudentTrackingSystem/StudentTrackingSystemApp/views.py
+++ StudentTrackingSystem/StudentTrackingSystemApp/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 
-def loginPage(request):
-	if request.method == 'POST':
-		email = request.POST.get('email')
-		password = request.POST.get('password')
 
-		print(f'{email} : {password}')
-		
-	context = {}
-	return render(request, 'StudentTrackingSystemApp/login.html', context)
+def loginPage(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        print(f'{email} : {password}')
+
+    context = {}
+    return render(request, 'StudentTrackingSystemApp/login.html', context)
+
 
 def registerPage(request):
-	context = {}
-	return render(request, 'StudentTrackingSystemApp/register.html', context)
+    context = {}
+    return render(request, 'StudentTrackingSystemApp/register.html', context)
+
+#able to read in files
+def homePage(request):
+    if request.method == 'POST':
+		# files will hold all the files that are read in
+        files = request.FILES.getlist('input_files')
+
+    context = {}
+    return render(request, 'StudentTrackingSystemApp/homepage.html', context)
