b1d44b96ea2a4bd2416cfbcb1f151ace470bb94a
Elliot-Chin <48703126+Elliot-Chin@users.noreply.github.com>
2021-10-14 09:34:44 -0300

    Added path for each different data views

--- StudentTrackingSystem/StudentTrackingSystemApp/urls.py
+++ StudentTrackingSystem/StudentTrackingSystemApp/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('login/', views.loginPage, name='login'),
     path('register/', views.registerPage, name='register'),
     path('homepage/', views.homePage, name='homepage'),
-     path('view_data/', views.view_data, name = 'view_data')
+    path('student_data/', views.student_data, name = 'student_data'),
+    path('course_data/', views.course_data, name = 'course_data'),
+    path('enrolment_data/', views.enrolment_data, name = 'enrolment_data')
 ]
