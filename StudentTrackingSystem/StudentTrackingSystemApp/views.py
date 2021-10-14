f8ed4787740c0d1eeb9d72fb69410847f13bea06
Justen Di Ruscio <justend29@gmail.com>
2021-10-14 20:12:53 -0300

    move functions into a class for lifetime mgmt.

--- StudentTrackingSystem/StudentTrackingSystemApp/views.py
+++ StudentTrackingSystem/StudentTrackingSystemApp/views.py
@@ -2,68 +2,70 @@ from django.contrib.auth import authenticate, get_user_model, logout
 from django.shortcuts import redirect, render
 from users.managers import UserManager
 from users.roles import UserRole
-from dataloader.load_extract import _uploadAllFiles
+from dataloader.load_extract import DataFileExtractor
 
 
 def registerPage(request):
-	context = {}
-	if request.method == 'POST':
-		email = request.POST.get('email')
-		password = request.POST.get('psw')
-		try:
-			# This is most likely temporary as we will 
-			# use Justen's models I assume?
-			User = get_user_model()
-			user = User.objects.create_user(
-				email=email,
-				username=email,
-				password=password,
-				role=UserRole.ProgramAdvisor,
-			)
-			return redirect('loginPage')
-		except Exception as e:
-			print(f'ERROR: {e}')
+    context = {}
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("psw")
+        try:
+            # This is most likely temporary as we will
+            # use Justen's models I assume?
+            User = get_user_model()
+            user = User.objects.create_user(
+                email=email,
+                username=email,
+                password=password,
+                role=UserRole.ProgramAdvisor,
+            )
+            return redirect("loginPage")
+        except Exception as e:
+            print(f"ERROR: {e}")
 
-	return render(request, 'StudentTrackingSystemApp/register.html', context)
+    return render(request, "StudentTrackingSystemApp/register.html", context)
 
 
 def loginPage(request):
-	context = {}
-	if request.method == 'POST':
-		email = request.POST.get('email')
-		password = request.POST.get('password')
-		try:
-			user = authenticate(request, username=email, password=password)
-			if user is not None:
-				print(f'Successfully logged in user: {user}')
-				# Here is where we need to redirect the user to landing page
-				return redirect('homepage')
-			else:
-				print(f'{user} does not exist')
-				return redirect('registerPage')
-		except Exception as e:
-			print(f'ERROR: {e}')
-	
-	return render(request, 'StudentTrackingSystemApp/login.html', context)
+    context = {}
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        try:
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                print(f"Successfully logged in user: {user}")
+                # Here is where we need to redirect the user to landing page
+                return redirect("homepage")
+            else:
+                print(f"{user} does not exist")
+                return redirect("registerPage")
+        except Exception as e:
+            print(f"ERROR: {e}")
 
-#able to read in files
+    return render(request, "StudentTrackingSystemApp/login.html", context)
+
+
+# able to read in files
 def homePage(request):
 
-    if request.method == 'POST':
-		# files will hold all the files that are read in
-        files = request.FILES.getlist('input_files')
+    if request.method == "POST":
+        # files will hold all the files that are read in
+        files = request.FILES.getlist("input_files")
         for f in files:
-            if f.name == 'personData.txt':
+            if f.name == "personData.txt":
                 personData = f
-            elif f.name == 'courseData.txt':
+            elif f.name == "courseData.txt":
                 courseData = f
-            elif f.name == 'transferData.txt':
+            elif f.name == "transferData.txt":
                 transferData = f
-	
+
+		uploader = DataFileExtractor()
         _uploadAllFiles(personData, courseData, transferData)
 
     context = {}
-    return render(request, 'StudentTrackingSystemApp/homepage.html', context)
+    return render(request, "StudentTrackingSystemApp/homepage.html", context)
 
 
 def student_data(request):
@@ -71,21 +73,18 @@ def student_data(request):
 
     all_entries = Student.objects.all()
     print(all_entries)
-    context = {
-        "object_list": all_entries
+    context = {"object_list": all_entries}
+    return render(request, "StudentTrackingSystemApp/Student_data.html", context)
 
-    }
-    return render(request, 'StudentTrackingSystemApp/Student_data.html', context)
 
 def course_data(request):
     from datamodel.models import Course
 
     all_entries = Course.objects.all()
     print(all_entries)
-    context = {
-        "object_list": all_entries
-    }
-    return render(request, 'StudentTrackingSystemApp/Course_data.html', context)
+    context = {"object_list": all_entries}
+    return render(request, "StudentTrackingSystemApp/Course_data.html", context)
+
 
 def enrolment_data(request):
     from datamodel.models import Enrolment
@@ -98,26 +97,22 @@ def enrolment_data(request):
     # student_number_list = []
     # course_code_list = []
     # course_name_list = []
-	
-    # for s in students_list_student_number:
-	#     student_number_list.append((s.get('student__student_number')))
 
+    # for s in students_list_student_number:
+    #     student_number_list.append((s.get('student__student_number')))
 
     # for c in course_list_course_code:
-	#     course_code_list.append((c.get('course__course_code')))
+    #     course_code_list.append((c.get('course__course_code')))
 
     # for c in course_list_course_name:
-	#     course_code_list.append((c.get('course__course_name')))
+    #     course_code_list.append((c.get('course__course_name')))
 
     print(all_entries)
-	
+
     context = {
-		"all_objects" : all_entries,
-		# "student_numbers" : student_number_list,
-		# "course_code" : course_code_list,
-		# "course_name" : course_name_list
+        "all_objects": all_entries,
+        # "student_numbers" : student_number_list,
+        # "course_code" : course_code_list,
+        # "course_name" : course_name_list
     }
-    return render(request, 'StudentTrackingSystemApp/enrolment_data.html', context)
-    
-
-
+    return render(request, "StudentTrackingSystemApp/enrolment_data.html", context)
