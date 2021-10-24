275ba418df722bf69cb2dcb4f15da822f6ef8807
Elliot-Chin <48703126+Elliot-Chin@users.noreply.github.com>
2021-10-24 14:37:43 -0300

    Added PreReq model

--- StudentTrackingSystem/datamodel/models.py
+++ StudentTrackingSystem/datamodel/models.py
@@ -13,8 +13,6 @@ class Student(models.Model):
     id = models.BigAutoField(primary_key=True)
     student_number = models.IntegerField()
     name = models.TextField(max_length=70)
-    gender = models.CharField(max_length=1)
-    address = models.TextField(max_length=140)
     email = models.EmailField(max_length=50)
     campus = models.CharField(max_length=2)
     program = models.CharField(max_length=10)
@@ -78,3 +76,7 @@ class Enrolment(models.Model):
 
     def __str__(self):
         return f"Enrolment: {self.id} (Student: {self.student.student_number}) (Course Section: {self.course.section})"
+
+class PreReq(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    course_code = models.CharField(max_length=10)
