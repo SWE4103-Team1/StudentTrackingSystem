e81195fb6bcfc4dec32e4980a54d94deb8308453
EthanG78 <ethangarnier78@gmail.com>
2021-10-28 12:34:49 -0300

    Rewrote more extensive unit tests for rank calculations

--- StudentTrackingSystem/StudentTrackingSystemApp/tests/test_rank.py
+++ StudentTrackingSystem/StudentTrackingSystemApp/tests/test_rank.py
@@ -1,12 +1,130 @@
 from django.test import TestCase
 
-from ..rankings import calculateRank
-from datamodel.tests.test_enrolment import EnrolmentTests
+from ..rankings import calculateRank, prereq
+from datamodel.models import Enrolment, Course
+from datamodel.tests.test_uploadset import UploadSetTests
+from datamodel.tests.test_student import StudentTests
 
 class RankTests(TestCase):
-	def test_calculate_rank(self):
-		enrollment_tester = EnrolmentTests()
-		enrollment = enrollment_tester.test_create_enrolment()
+	def test_calculate_rank_fir(self):
+		us_tester = UploadSetTests()
+		student_tester = StudentTests()
+
+		us = us_tester.test_create_null_upload_set()
+		student = student_tester.test_create_student()
+
+		for course_id in prereq:
+
+			if prereq[course_id] != 'FIR':
+
+				c = Course(
+					course_code=course_id,
+					section="FR01B",
+					credit_hours=3,
+					name="TEST",
+					upload_set=us,
+				)
+				c.save()
+
 	
-		self.assertTrue(calculateRank(enrollment.student.id) == 'FIR')
+				e = Enrolment(
+				    student=student,
+				    course=c,
+				    term="FR/2021",
+				    grade="B-",
+				    upload_set=us,
+				)
+				e.save()
+
+		self.assertTrue(calculateRank(student.id) == 'FIR')
+
+	def test_calculate_rank_jun(self):
+		us_tester = UploadSetTests()
+		student_tester = StudentTests()
+
+		us = us_tester.test_create_null_upload_set()
+		student = student_tester.test_create_student()
+
+		for course_id in prereq:
+
+			if prereq[course_id] != 'JUN':
+				c = Course(
+					course_code=course_id,
+					section="FR01B",
+					credit_hours=3,
+					name="TEST",
+					upload_set=us,
+				)
+				c.save()
+	
+				e = Enrolment(
+				    student=student,
+				    course=c,
+				    term="FR/2021",
+				    grade="B-",
+				    upload_set=us,
+				)
+				e.save()
+
+		self.assertTrue(calculateRank(student.id) == 'JUN')
+
+	def test_calculate_rank_sop(self):
+		us_tester = UploadSetTests()
+		student_tester = StudentTests()
+
+		us = us_tester.test_create_null_upload_set()
+		student = student_tester.test_create_student()
+
+		for course_id in prereq:
+
+			if prereq[course_id] != 'SOP':
+				c = Course(
+					course_code=course_id,
+					section="FR01B",
+					credit_hours=3,
+					name="TEST",
+					upload_set=us,
+				)
+				c.save()
+	
+				e = Enrolment(
+				    student=student,
+				    course=c,
+				    term="FR/2021",
+				    grade="B-",
+				    upload_set=us,
+				)
+				e.save()
+
+		self.assertTrue(calculateRank(student.id) == 'SOP')
+
+	def test_calculate_rank_sen(self):
+		us_tester = UploadSetTests()
+		student_tester = StudentTests()
+
+		us = us_tester.test_create_null_upload_set()
+		student = student_tester.test_create_student()
+
+		for course_id in prereq:
+
+			if prereq[course_id] != 'SEN':
+				c = Course(
+					course_code=course_id,
+					section="FR01B",
+					credit_hours=3,
+					name="TEST",
+					upload_set=us,
+				)
+				c.save()
+	
+				e = Enrolment(
+				    student=student,
+				    course=c,
+				    term="FR/2021",
+				    grade="B-",
+				    upload_set=us,
+				)
+				e.save()
+
+		self.assertTrue(calculateRank(student.id) == 'SEN')
 
