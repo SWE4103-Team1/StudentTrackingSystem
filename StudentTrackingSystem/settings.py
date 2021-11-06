8adc34c3c84acf7c03928760edd0527c0078e0e5
CarterMacLennan <cartermaclennannn@gmail.com>
2021-11-06 01:41:23 -0300

    modified folder hierarchy for AWS EB

--- StudentTrackingSystem/settings.py
+++ StudentTrackingSystem/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-7uh^l%zllqn=^ujk_g_szhj#!f+vahxzw*w2)hx=_*^(8wpb)p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eb-django-app-dev.elasticbeanstalk.com']
 
 
 # Application definition
