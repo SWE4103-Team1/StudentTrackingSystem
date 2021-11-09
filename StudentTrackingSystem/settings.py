5c69fdfd47f8d1a166c7156907b0459f4d595bf3
Carter MacLennan <60050873+CarterMacLennan@users.noreply.github.com>
2021-11-09 12:41:57 -0400

    Update settings.py

--- StudentTrackingSystem/settings.py
+++ StudentTrackingSystem/settings.py
@@ -140,8 +140,8 @@ USE_TZ = True
 
 
 # STATIC_URL = "static/"
-STATIC_URL = "/static/"
-# STATIC_ROOT = "static"
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 STATICFILES_DIRS = [os.path.join(BASE_DIR / "static")]
 
 # Default primary key field type
