96048e63ee7cd43d57a8812b1c103f5e67bb01fd
Yuzhuo Zheng <yzheng10@unb.ca>
2021-11-09 12:24:22 -0400

    Static_URL

--- StudentTrackingSystem/settings.py
+++ StudentTrackingSystem/settings.py
@@ -139,8 +139,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATIC_URL = "static/"
-
+# STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 STATICFILES_DIRS = [os.path.join(BASE_DIR / "static")]
 
 # Default primary key field type
