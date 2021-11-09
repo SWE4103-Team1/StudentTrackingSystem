5396ab22196dc81e5201ff507f434df2e11d9a8a
Yuzhuo Zheng <yzheng10@unb.ca>
2021-11-09 12:15:47 -0400

    11.9

--- StudentTrackingSystem/settings.py
+++ StudentTrackingSystem/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -27,7 +26,7 @@ SECRET_KEY = "django-insecure-7uh^l%zllqn=^ujk_g_szhj#!f+vahxzw*w2)hx=_*^(8wpb)p
 DEBUG = True
 
 ALLOWED_HOSTS = ['swe4103-env.eba-irrkpdyi.us-east-2.elasticbeanstalk.com']
-
+# ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -77,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "StudentTrackingSystem.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -98,12 +96,12 @@ else:
         }
     }
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
-#}
+# }
 
 
 # Password validation
@@ -124,7 +122,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -138,14 +135,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATIC_URL = "StudentTrackingSystemApp/static/"
+STATIC_URL = "static/"
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR / "StudentTrackingSystemApp", "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR / "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
