f2e266e4e167e0c09b73b009396cbeac4aadfd91
Yuzhuo <71301573+similato87@users.noreply.github.com>
2021-11-10 14:28:49 -0400

    Update settings.py

--- StudentTrackingSystem/settings.py
+++ StudentTrackingSystem/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = "django-insecure-7uh^l%zllqn=^ujk_g_szhj#!f+vahxzw*w2)hx=_*^(8wpb)p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['swe4103-env.eba-irrkpdyi.us-east-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['elasticbeanstalk.com']
+
 # ALLOWED_HOSTS = []
 
 # Application definition
